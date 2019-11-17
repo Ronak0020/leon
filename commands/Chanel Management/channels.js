030b97d7c12c1d54aed28a9b8f862f0e4cba7819
Ronak0020 <49647021+Ronak0020@users.noreply.github.com>
2019-11-17 10:53:56 +0530

    Update channels.js

--- commands/Chanel Management/channels.js
+++ commands/Chanel Management/channels.js
@@ -14,7 +14,7 @@ exports.create = {
     if (!checkPermission(msg)) {
       return;
     }
-		msg.guild.createChannel(suffix, { type: 'text' }).then(function(channel) {
+		msg.guild.createChannel(suffix, { 'type': 'text' }).then(function(channel) {
 			msg.channel.send("created text channel " + channel);
 		}).catch(function(error){
 			msg.channel.send("failed to create channel: **" + error + "**");
